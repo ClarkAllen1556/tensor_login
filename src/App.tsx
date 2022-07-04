beafd804961ce9481f1658ae9febad4006ad8587
AC1556 <allen@urbanlife.me>
2022-07-04 16:23:28 +0900

    removing staring code

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,20 @@
 import { useState } from 'react';
-import logo from '@imgs/logo.svg';
+
 import '@styles/App.css';
 
+import Input from '@components/input/Input';
+import Button from '@components/button/Button';
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState<string>('');
+  function handleChange(value: string) {
+    setMessage(value);
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Input type="text" change={(e) => handleChange(e.target.value)} />
+      <Button click={() => alert(message)}>Show message!</Button>
     </div>
   );
 }
