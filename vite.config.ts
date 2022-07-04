11e732c9d83ba67ebc7b40d82db33826d2887b03
AC1556 <allen@urbanlife.me>
2022-07-04 14:05:30 +0900

    autoformatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import * as path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@styles": path.resolve(__dirname, "./src/assets/styles"),
-      "@public": path.resolve(__dirname, "./src/public"),
-      "@": path.resolve(__dirname, "./src"),
+      '~': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@styles': path.resolve(__dirname, './src/assets/styles'),
+      '@public': path.resolve(__dirname, './src/public'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
